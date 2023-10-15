0f46edf954763e41644eebbfd1aeda162ff56a61
j19015 <j19015@sangi.jp>
2023-10-16 02:14:34 +0900

    [add] 一覧、詳細取得処理

--- libs/client.ts
+++ libs/client.ts
@@ -1,5 +1,11 @@
 import { createClient } from 'microcms-js-sdk';
 
+export type Blog = {
+    id: string;
+    title: string;
+    body: string;   
+}
+
 if (!process.env.SERVICE_DOMAIN) {
     throw new Error("MICROCMS_SERVICE_DOMAIN is required");
 }
@@ -9,6 +15,23 @@ if (!process.env.API_KEY) {
 }
 
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN,
-  apiKey: process.env.API_KEY,
+    serviceDomain: process.env.SERVICE_DOMAIN,
+    apiKey: process.env.API_KEY,
 });
+
+// ブログ一覧を取得
+export const getBlogs = async () => {
+    const blogs = await client.getList<Blog>({
+    endpoint: "blogs"
+    });
+    return blogs;
+}
+
+// ブログの詳細を取得
+export const getDetail = async (contentId: string) => {
+    const blog = await client.getListDetail<Blog>({
+        endpoint: "blogs",
+        contentId,
+    });
+    return blog;
+};
